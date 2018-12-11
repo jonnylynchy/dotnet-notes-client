6d693442c38b2253bd0e8fde94134444c58408b9
Jon Lynch <jonnylynchy@gmail.com>
2018-12-10 21:38:52 -0600

    committing all the things!!!!

--- src/App.js
+++ src/App.js
@@ -1,28 +1,177 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Alert, Button, InputGroup, Input, InputGroupAddon } from 'reactstrap';
+
+import Note from './components/Note';
+
 import './App.css';
 
+const rootPath = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+const rootUrl = `${rootPath}/api/notes/`
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		notes: [],
+		newNote: '',
+		updated: false
+	};
+
+	componentDidMount() {
+		this.fetchNotes();
+	}
+
+	fetchNotes = _ => {
+		fetch(rootUrl)
+      		.then(res => res.json())
+			.then(json => {
+				console.log(json);
+				this.setState({ notes: json });
+			});
+	}
+
+	noteChange = (e) => {
+		const note = e.target.value;
+		this.setState({
+			newNote: note
+		});
+	}
+
+	updateNote = async (noteId, text) => {
+		// update UI (expecting that update on server worked)
+		const updatedNotes = this.state.notes.map((x) => {
+			if(x.noteID === noteId) {
+				const newNote = {...x, noteText: text}
+				return newNote;
+			}
+			return x;
+		});
+
+		this.setState({
+			notes: updatedNotes
+		});
+
+		// send update to server
+		const noteUrl = rootUrl + noteId;
+		console.log(noteUrl);
+		const data = {
+			NoteText: text
+		};
+		const rawResponse = await fetch(noteUrl, {
+			method: 'PUT',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		});
+
+		if(rawResponse.ok) {
+			console.log('note updated');
+		} else {
+			console.log('Error', rawResponse.statusText);
+		}
+	}
+
+	insertNote = async _ => {
+		const data = {
+			NoteText: this.state.newNote
+		};
+		const rawResponse = await fetch(rootUrl, {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		});
+		if(rawResponse.ok) {
+			console.log('note inserted');
+			this.setState({
+				updated: true
+			});
+			this.fetchNotes();
+		} else {
+			this.setState({
+				error: rawResponse.statusText
+			});
+		}
+	}
+
+	deleteNote = async (noteId) => {
+		// update UI (expecting that update on server worked)
+		const updatedNotes = this.state.notes.filter((x) => x.noteID !== noteId);
+
+		this.setState({
+			notes: updatedNotes
+		});
+
+		// send update to server
+		const noteUrl = rootUrl + noteId;
+		const rawResponse = await fetch(noteUrl, {
+			method: 'DELETE',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		});
+
+		if(rawResponse.ok) {
+			console.log('note deleted');
+		} else {
+			console.log('Error', rawResponse.statusText);
+		}
+	}
+
+	render() {
+
+		const noteList = this.state.notes.map((note) => <Note id={note.noteID} key={note.noteID} note={note} updateNote={this.updateNote} deleteNote={this.deleteNote}/>);
+
+		return (
+			<div className="App">
+				<div className="container">
+					<header>
+						Scratch
+					</header>
+					{this.state.updated
+						? (
+							<Alert color="success">
+								Note Added
+						  	</Alert>
+						  )
+						: null
+					}
+					{this.state.error 
+						? (
+							<Alert color="danger">
+								{this.state.error}
+							</Alert>
+						)
+						: null
+					}
+					<p></p>
+					<InputGroup>
+						<Input 
+							onChange={this.noteChange}
+							value={this.state.newNote}
+							placeholder="Enter a new note and click 'Save'"
+						/>
+						<InputGroupAddon addonType="append">
+							<Button 
+								onClick={this.insertNote} 
+								color="primary">
+								Save
+							</Button>
+						</InputGroupAddon>
+					</InputGroup>
+					<p></p>
+					<h3 className="text-center">Notes</h3>
+					<div className="note-list text-center">
+						{noteList.length ? noteList : <div>No notes</div>}
+					</div>
+					
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Note.js
@@ -0,0 +1,56 @@
+import React, { PureComponent } from 'react';
+import { Card, CardText, CardBody, NavLink, Input } from 'reactstrap';
+
+class Note extends PureComponent {
+    state = {
+        editMode: false,
+        noteText: this.props.note.noteText
+    }
+    
+    editNote = (e) => {
+        console.log('edit note', this.state.noteText);
+        this.setState({
+            editMode: !this.state.editMode
+        },  _ => {
+            if(!this.state.editMode) {
+                this.props.updateNote(this.props.id, this.state.noteText);
+            }
+        });
+    }
+
+    deleteNote = (e) => {
+        this.props.deleteNote(this.props.id);
+    }
+
+    noteChange = (e) => {
+        this.setState({
+            noteText: e.target.value
+        });
+    }
+
+    completeNote = (e) => {
+        console.log('complete note');
+    }
+
+    render() {
+        const cardText = this.state.editMode
+            ? <Input value={this.state.noteText} onChange={this.noteChange} />
+            : <CardText>{this.props.note.noteText}</CardText>;
+
+        const editLinkClass = this.state.editMode
+        ? 'fa-check-square'
+        : 'fa-edit';
+
+        return (
+            <Card className="note">
+                <CardBody>
+                    <i className="far fa-window-close fa-lg" onClick={this.deleteNote}></i>
+                    <i className={`fas ${editLinkClass} fa-lg`} onClick={this.editNote}></i>
+                    <div className="note-text">{cardText}</div>
+                </CardBody>
+            </Card>
+        );
+    }
+}
+
+export default Note;
